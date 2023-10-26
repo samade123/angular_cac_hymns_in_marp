1f9c1d64c0c3a6218ee77018628e6863acf4346a
Samuel Ademola <samuel.ademola43@gmail.com>
2023-10-26 23:47:15 +0100

    remove app-routing &add notion service init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GrabNotiondbService } from './grab-notiondb.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  // imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule],
+  providers: [GrabNotiondbService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

c2b642425a998f337d0a45a9e072e29f64eced2e
Samuel Ademola <samuel.ademola43@gmail.com>
2023-10-29 16:48:21 +0000

    removed custom webpack

--- extra-webpack.config.ts
+++ extra-webpack.config.ts
@@ -1,45 +1,24 @@
-const CopyPlugin = require("copy-webpack-plugin");
-
-// module.exports = {
-//   plugins: [
-//     new CopyPlugin({
-//       patterns: [
-//         { from: "src/api", to: "../api" },
-//       ],
-//     }),
-//   ],
-// };
+const CopyPlugin = require('copy-webpack-plugin');
 
 import * as webpack from 'webpack';
-import { CustomWebpackBrowserSchema, TargetOptions } from '@angular-builders/custom-webpack';
+import {
+  CustomWebpackBrowserSchema,
+  TargetOptions,
+} from '@angular-builders/custom-webpack';
 
 export default (
   config: webpack.Configuration,
   options: CustomWebpackBrowserSchema,
   targetOptions: TargetOptions
 ) => {
-  if (config.module && config.module.rules) {
-    config.module.rules.push(
-      {
-        test: /\.gif$/,
-        loader: 'your-custom-loader',
-        options: {
-          yourOption: false
-        }
-      }
-    );
-  }
-
-  // or if you need plugins
-  if (config.plugins) {
-    config.plugins.push(
-      new CopyPlugin({
-        patterns: [
-          { from: "src/api", to: "../../api" },
-        ],
-      }),);
-  }
-
+//   // or if you need plugins
+//   if (config.plugins) {
+//     config.plugins.push(
+//       new CopyPlugin({
+//         patterns: [{ from: 'src/api', to: '../api' }],
+//       })
+//     );
+//   }
 
   return config;
-}
+};

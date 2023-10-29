dad1c730278c1179fe5b2c951d239452e2f94971
Samuel Ademola <samuel.ademola43@gmail.com>
2023-10-29 22:30:25 +0000

    attempted to getMarpfile but we have used a fetch instead in previous commit

--- src/app/grab-notiondb.service.ts
+++ src/app/grab-notiondb.service.ts
@@ -1,24 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
-import { StorageManagerService } from './storage-manager.service';
-
-export interface SimpleHymn {
-  id: string;
-  name: string;
-  last_edited_by: string;
-}
+import { Result, SimpleHymn, RequestOptions } from './test-interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GrabNotiondbService {
   constructor(
-    private http: HttpClient,
-    // private storageManagerService: StorageManagerService
+    private http: HttpClient // private storageManagerService: StorageManagerService
   ) {}
-
-
 
   getFunctionData(functionName: string) {
     return this.http.get(`${functionName}`).pipe(
@@ -91,22 +82,19 @@ export class GrabNotiondbService {
     );
   }
 
-  //   simplifyHymns(results: NotionPage[]): SimpleHymn[] {
-  //     return results.map((result) => {
-  //       // let name: string | notion.TitleProperty = result.properties.Name;
-  //       const name: ResultsProperty  = result.properties
-  //       // const name: ResultsProperty  = result.properties.Name.rich_text[0].text.content;
+  async getMarp(s3Url: string): Promise<string> {
+    const requestOptions: RequestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
 
-  //       const last_edited_by = result.last_edited_by.id;
+    try {
+      const response = await fetch(s3Url, requestOptions);
+      const result = await response.text();
 
-  //       return {
-  //         id: result.id,
-  //         name,
-  //         last_edited_by,
-  //       };
-  //     });
-  //   }
-  //   private isTitleProperty(property: any): property is notion.TitleProperty {
-  //     return property.type === 'title';
-  //   }
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
 }

8be70b0b24ce82f48195ae3b3c69302c36a65fe4
Samuel Ademola <samuel.ademola43@gmail.com>
2023-11-06 23:32:09 +0000

    adde button filters with new hymn selector lists

--- src/app/hymn-sidebar/hymn-sidebar.component.ts
+++ src/app/hymn-sidebar/hymn-sidebar.component.ts
@@ -1,8 +1,13 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { SimpleHymn } from './../test-interface';
+import { SimpleHymn, SimpleHymnItem } from './../test-interface';
 import { GrabNotiondbService } from '../services/grab-notiondb.service';
+import { IndexDbManagerService } from '../services/index-db-manager.service';
 import { animate } from "popmotion"
 
+export interface ButtonFilters {
+  name: string;
+  selected: boolean;
+}
 @Component({
   selector: 'app-hymn-sidebar',
   templateUrl: './hymn-sidebar.component.html',
@@ -15,10 +20,28 @@ export class HymnSidebarComponent implements OnInit {
   ) {}
   @Input() simpleHymns: SimpleHymn[];
   @Output() selectedHymnId = new EventEmitter<string>();
+  hymnItemsArr: SimpleHymnItem[];
+  allSimpleHymns: SimpleHymn[];
   hymnTrackbyFn = this.notionService.simpleHymnsTrackByFn;
+  simpleHymnItemsTrackByFn = this.notionService.simpleHymnItemsTrackByFn;
+  buttonFilters: ButtonFilters[] = [
+    { name: 'Most recent', selected: true },
+    { name: 'All Hymns', selected: false },
+  ];
+  inputFocusBgDeg = '180deg'
 
   ngOnInit(): void {
     console.log('here');
+    this.dBstorageServie.getLastFiveHymns().then((arr) => {
+      this.hymnItemsArr = [...arr];
+    });
+    this.dBstorageServie.returnAll().then((arr) => {
+      this.allSimpleHymns = [...arr];
+    });
+  }
+  filterHymns(buttonFilter: ButtonFilters): void {
+    this.buttonFilters.forEach((btn) => (btn.selected = false));
+    buttonFilter.selected = true;
   }
 
   pickHymn(hymnId: string): void {

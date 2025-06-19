acb8eb4a9ef48a12da186e92527f7b2d4fc570ff
samade123 <samuel.ademola43@gmail.com>
2025-06-19 19:42:39 +0100

    interface formatting

--- src/app/test-interface.ts
+++ src/app/test-interface.ts
@@ -1,51 +1,50 @@
 export interface NotionDBQuery {
-  object:           string;
-  results:          Result[];
-  next_cursor:      null;
-  has_more:         boolean;
-  type:             string;
+  object: string;
+  results: Result[];
+  next_cursor: null;
+  has_more: boolean;
+  type: string;
   page_or_database: PageOrDatabase;
-  request_id:       string;
+  request_id: string;
 }
 
-export interface PageOrDatabase {
-}
+export interface PageOrDatabase {}
 
 export interface Result {
-  object:           string;
-  id:               string;
-  created_time:     Date;
+  object: string;
+  id: string;
+  created_time: Date;
   last_edited_time: Date;
-  created_by:       TedBy;
-  last_edited_by:   TedBy;
-  cover:            null;
-  icon:             null;
-  parent:           Parent;
-  archived:         boolean;
-  properties:       Properties;
-  url:              string;
-  public_url:       null;
+  created_by: TedBy;
+  last_edited_by: TedBy;
+  cover: null;
+  icon: null;
+  parent: Parent;
+  archived: boolean;
+  properties: Properties;
+  url: string;
+  public_url: null;
 }
 
 export interface TedBy {
   object: string;
-  id:     string;
+  id: string;
 }
 
 export interface Parent {
-  type:        string;
+  type: string;
   database_id: string;
 }
 
 export interface Properties {
-  "Files & media": FilesMedia;
-  Name:            Name;
-  Number:          Number;
+  'Files & media': FilesMedia;
+  Name: Name;
+  Number: Number;
 }
 
 export interface FilesMedia {
-  id:    string;
-  type:  string;
+  id: string;
+  type: string;
   files: FileElement[];
 }
 
@@ -56,41 +55,41 @@ export interface FileElement {
 }
 
 export interface FileFile {
-  url:         string;
+  url: string;
   expiry_time: Date;
 }
 
 export interface Name {
-  id:        string;
-  type:      string;
+  id: string;
+  type: string;
   rich_text: RichText[];
 }
 
 export interface RichText {
-  type:        string;
-  text:        Text;
+  type: string;
+  text: Text;
   annotations: Annotations;
-  plain_text:  string;
-  href:        null;
+  plain_text: string;
+  href: null;
 }
 
 export interface Annotations {
-  bold:          boolean;
-  italic:        boolean;
+  bold: boolean;
+  italic: boolean;
   strikethrough: boolean;
-  underline:     boolean;
-  code:          boolean;
-  color:         string;
+  underline: boolean;
+  code: boolean;
+  color: string;
 }
 
 export interface Text {
   content: string;
-  link:    null;
+  link: null;
 }
 
 export interface Number {
-  id:    string;
-  type:  string;
+  id: string;
+  type: string;
   title: RichText[];
 }
 
